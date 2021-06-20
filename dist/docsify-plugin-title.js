dfc8618adc73b87676e556532605590c63ace9d3
Sujaykumar Hublikar <hello@sujaykumarh.com>
2021-06-20 20:44:44 +0530

    Initial Commit

---
+++ dist/docsify-plugin-title.js
@@ -0,0 +1,36 @@
+/*
+ * docsify-plugin-title.js v0.0.1
+ * (https://github.com/Sujaykumarh/docsify-plugin-title/)
+ * Copyright (c) 2021 Sujaykumar.Hublikar <hello@sujaykumarh.com>
+ * Github (@sujaykumarh)
+ * Licensed under Apache License, Version 2.0
+ */
+
+
+// javascript policy
+'use strict';
+
+
+// main function
+function customPageTitle( hook, vm ) {
+
+	// get the `@customPageTitle:` data
+	hook.beforeEach( function( content ) {
+    });
+
+    // set page title
+	hook.doneEach( function() {
+        console.log(document.title);
+    });
+}
+
+
+// find heading plugin options
+window.$docsify.customPageTitle = Object.assign(
+	autoHeaderOptions,
+	window.$docsify.customPageTitle
+);
+window.$docsify.plugins = [].concat(
+	customPageTitle,
+	window.$docsify.plugins
+);
