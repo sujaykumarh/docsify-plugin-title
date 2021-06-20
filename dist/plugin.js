425de5b39df2855dc263bb4992d64f923d2516ab
Sujaykumar Hublikar <hello@sujaykumarh.com>
2021-06-20 23:55:10 +0530

    v1.0.3

---
+++ dist/plugin.js
@@ -0,0 +1,25 @@
+/*
+ * docsify-plugin-title/plugin.js v1.0.3
+ * (https://github.com/Sujaykumarh/docsify-plugin-title/)
+ *
+ * Copyright (c) 2021 Sujaykumar.Hublikar <hello@sujaykumarh.com>
+ * Github (@sujaykumarh)
+ *
+ * @license
+ * Licensed under Apache License, Version 2.0
+ */
+// javascript policy
+"use strict";
+// config options
+const customPageTitleOptions={prefix:false,suffix:false,seprator:"|",debug:false};
+// main function
+function customPageTitle(hook,vm){
+// before hook
+hook.beforeEach((function(content){}));
+// after hook
+hook.doneEach((function(){var _title=document.title;// title from docsify
+debug("customPageTitleOptions: "+customPageTitleOptions);debug("page title [before]: "+document.title);if(customPageTitleOptions.prefix!=""||customPageTitleOptions.prefix!=false){_title=customPageTitleOptions.prefix+" "+customPageTitleOptions.seprator+" "+_title;debug("new title [prefix]:"+_title)}if(customPageTitleOptions.suffix!=""||customPageTitleOptions.suffix!=false){_title=_title+" "+customPageTitleOptions.seprator+" "+customPageTitleOptions.suffix;debug("new title [suffix]:"+_title)}document.title=_title}))}function debug(msg){if(customPageTitleOptions.debug){console.log(msg)}}function error(msg){if(customPageTitleOptions.debug){console.error(msg)}}
+// find customPageTitle plugin options
+window.$docsify.customPageTitle=Object.assign(customPageTitleOptions,window.$docsify.customPageTitle);
+// Set docsify plugin
+window.$docsify.plugins=[].concat(customPageTitle,window.$docsify.plugins);
